8f986c4c12f153fb1c72ae56a671df3c8819aa8b
Rachanaa30 <73883944+Rachanaa30@users.noreply.github.com>
2021-08-07 13:12:15 +0530

    Add files via upload

---
+++ Media Player/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.mediaplayer;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.concurrent.Semaphore;
+
+public class MainActivity extends AppCompatActivity {
+ MediaPlayer player;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void play(View v) {
+        if (player == null) {
+            player = MediaPlayer.create(this,R.raw.song);
+            player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    stopPlayer();
+                }
+            });
+        }
+
+        player.start();
+    }
+
+    public void pause(View v) {
+        if (player != null) {
+            player.pause();
+        }
+    }
+
+    public void stop(View v) {
+        stopPlayer();
+    }
+
+    private void stopPlayer() {
+        if (player != null) {
+            player.release();
+            player = null;
+            Toast.makeText(this, "MediaPlayer released", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopPlayer();
+    }
+}
